f0f428ace122513f3d61d2d3e7a54565c3270dca
PratibhaRepos1 <pratibha.jadhav3229@gmail.com>
2021-12-25 16:37:19 +0530

    project setup

--- AllInOne/src/app/app.component.ts
+++ AllInOne/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AllInOne';
+export class AppComponent implements OnInit{
+  title = 'Dockerized an Angular application';
+  public msg: any;
+  // tslint:disable-next-line:typedef
+  ngOnInit()
+  {
+    this.msg = 'Hello from Angular/Docker';
+  }
 }
