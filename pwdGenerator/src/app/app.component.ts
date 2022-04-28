edc465aba514d47395dc0378fda2d7b956f334e8
PratibhaRepos1 <pratibha.jadhav3229@gmail.com>
2022-04-28 12:55:57 +0530

    added change event and functions

--- pwdGenerator/src/app/app.component.ts
+++ pwdGenerator/src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pwdGenerator';
+  password = '';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+
+  onButtonClick() {
+    this.password = 'MY PASSWORD!!!';
+    console.log(this.includeLetters);
+    console.log(this.includeNumbers);
+    console.log(this.includeSymbols);
+  }
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+
+  }
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+
+  }
+  onChnageUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+
+  }
 }
