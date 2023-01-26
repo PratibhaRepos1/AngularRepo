f1d3549e2406eac91b4e718b44177f1d04ddea6b
PratibhaRepos1 <74100289+PratibhaRepos1@users.noreply.github.com>
2023-01-26 07:32:28 +0700

    created nav component and auh modal component

---
+++ clips/src/app/shared/modal/modal.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css'],
+//  providers: [ModalService]
+})
+export class ModalComponent implements OnInit {
+
+  constructor(public modal: ModalService) {
+
+  }
+
+  ngOnInit(): void {
+   // console.log(this.modal.visible)
+
+  }
+  closeModal() {
+    this.modal.toggleModal();
+  }
+
+}
