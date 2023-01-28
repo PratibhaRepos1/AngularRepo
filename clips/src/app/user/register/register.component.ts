389802de36c10a47bed544125e0e43ba53d62074
PratibhaRepos1 <74100289+PratibhaRepos1@users.noreply.github.com>
2023-01-28 21:05:41 +0700

    input component and name validation

--- clips/src/app/user/register/register.component.ts
+++ clips/src/app/user/register/register.component.ts
@@ -8,17 +8,28 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  registerForm = new FormGroup({
-    name: new FormControl('', [
+    name = new FormControl('', [
     Validators.required,
     Validators.minLength(3)
-    ]),
-    email: new FormControl(''),
-    age: new FormControl(''),
-    password: new FormControl(''),
-    confirm_password: new FormControl(''),
-    phoneNumber: new FormControl(''),
+    ]);
+    email = new FormControl('');
+    age = new FormControl('');
+    password = new FormControl('');
+    confirm_password = new FormControl('');
+    phoneNumber = new FormControl('');
 
-  })
+  registerForm = new FormGroup({
+   name: this.name,
+   email: this.email,
+   age: this.age,
+   password: this.password,
+   confirm_password: this.confirm_password,
+   phoneNumber: this.phoneNumber
+
+  });
+
+  // constructor() {
+  //   this.name
+  // }
 
 }
