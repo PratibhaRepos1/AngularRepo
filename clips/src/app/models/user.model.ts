305a82a6d6888abc33a821a87051f49cdd863a3b
PratibhaRepos1 <74100289+PratibhaRepos1@users.noreply.github.com>
2023-02-18 12:09:51 +0700

    refactor code - add auth service

---
+++ clips/src/app/models/user.model.ts
@@ -0,0 +1,20 @@
+
+/* interface Vs Class */
+
+export default interface IUser {
+  email:string,
+  password:string,
+  name: string,
+  phoneNumber: string,
+  age: number
+
+}
+
+// export default class IUser {
+//   email?: string;
+//   password?:string;
+//   name?: string;
+//   phoneNumber?: string;
+//   age?: number;
+
+// }
