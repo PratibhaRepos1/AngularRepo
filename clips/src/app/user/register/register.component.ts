90fb7088ba4df673bb3110c2996f3b2f4ecb0b3a
PratibhaRepos1 <74100289+PratibhaRepos1@users.noreply.github.com>
2023-02-27 08:19:20 +0700

    custom validations

--- clips/src/app/user/register/register.component.ts
+++ clips/src/app/user/register/register.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
-import  IUser from 'src/app/models/user.model';
+import IUser from 'src/app/models/user.model';
+import { RegisterValidators } from '../validators/register-validators';
+import { EmailTaken } from '../validators/email-taken';
 
 @Component({
   selector: 'app-register',
@@ -9,12 +11,18 @@ import  IUser from 'src/app/models/user.model';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService,
+    private emailTaken: EmailTaken
+    ) {}
   inSubmission = false;
 
   name = new FormControl('', [Validators.required, Validators.minLength(3)]);
-  email = new FormControl('', [Validators.required, Validators.email]);
-  age = new FormControl <number | null>(null, [
+  email = new FormControl('', [
+   Validators.required,
+   Validators.email
+  ], [this.emailTaken.validate]);
+
+  age = new FormControl<number | null>(null, [
     Validators.required,
     Validators.min(18),
     Validators.max(120),
@@ -34,14 +42,17 @@ export class RegisterComponent {
   alertColor = 'blue';
   alertMsg = 'please wait! Your account is being created.';
 
-  registerForm = new FormGroup({
-    name: this.name,
-    email: this.email,
-    age: this.age,
-    password: this.password,
-    confirm_password: this.confirm_password,
-    phoneNumber: this.phoneNumber,
-  });
+  registerForm = new FormGroup(
+    {
+      name: this.name,
+      email: this.email,
+      age: this.age,
+      password: this.password,
+      confirm_password: this.confirm_password,
+      phoneNumber: this.phoneNumber,
+    },
+    [RegisterValidators.match('password', 'confirm_password')]
+  );
 
   async register() {
     this.showAlert = true;
@@ -53,8 +64,6 @@ export class RegisterComponent {
 
     try {
       await this.auth.createUser(this.registerForm.value as IUser);
-
-
     } catch (e) {
       console.error(e);
       this.alertMsg = 'An unexpected error occur! Please try again';
