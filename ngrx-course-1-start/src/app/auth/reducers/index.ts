bea6be9d3e93e6beb265bfef23a545643e8c25ee
Pratibha <pratibhanilesh.jadhav@allianz.com>
2023-04-12 08:09:27 +0700

    ngrx in angular

---
+++ ngrx-course-1-start/src/app/auth/reducers/index.ts
@@ -0,0 +1,39 @@
+import {
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector,
+  createReducer,
+  createSelector,
+  MetaReducer,
+  on,
+  State
+} from '@ngrx/store';
+import { AuthActions } from '../actions-types';
+import { User } from '../model/user.model';
+//import { environment } from '../../../environments/environment';
+
+//export const authFeatureKey = 'auth';
+
+export interface AuthState {
+  user: User
+}
+
+export const initialAuthState: AuthState = {
+  user: undefined
+}
+
+// export const reducers: ActionReducerMap<AuthState> = {
+
+// };
+
+export const authReducer = createReducer(
+  initialAuthState,
+  on(AuthActions.login, (state,action) => {
+    return {
+      user: action.user
+    }
+  })
+)
+
+
+
