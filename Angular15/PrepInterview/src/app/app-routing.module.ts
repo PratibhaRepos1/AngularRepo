f63208412906b165d0d62bd238ebe34537c350a8
PratibhaRepos1 <74100289+PratibhaRepos1@users.noreply.github.com>
2023-07-27 15:23:08 +0700

    initial code

---
+++ Angular15/PrepInterview/src/app/app-routing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{ path: 'technologies', loadChildren: () => import('./technologies/technologies.module').then(m => m.TechnologiesModule) }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
