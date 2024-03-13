81cc9f0f9756c083b690c03b8767d3bc14e7f51b
PratibhaRepos1 <74100289+PratibhaRepos1@users.noreply.github.com>
2024-03-13 08:59:07 +0700

    data binding in angular

---
+++ Angular16/AngularApp/src/app/employee/employee.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Employee } from './employee';
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent {
+  showEdit: boolean = false;
+
+  employees: Employee[] = [
+    {
+      id: 1, firstName: 'John', lastName: 'Doe', salary: 50000, dob: '1990-01-01', email: 'john.doe@example.com'
+    },
+    {
+      id: 2, firstName: 'Jane', lastName: 'Smithh', salary: 60000, dob: '1995-05-15', email: 'jane.smith@example.com'
+    }
+  ];
+
+  EditEmp() {
+    this.showEdit = true;
+    console.log(this.showEdit);
+
+  }
+
+  UpdateEmp() {
+    this.showEdit = false;
+    console.log(this.showEdit);
+      
+  }
+}
