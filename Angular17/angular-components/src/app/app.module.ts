2aca2ace7f120f11798c9655dd859e7385366518
PratibhaRepos1 <74100289+PratibhaRepos1@users.noreply.github.com>
2024-05-07 12:14:59 +0700

    created Input() property 'course'

--- Angular17/angular-components/src/app/app.module.ts
+++ Angular17/angular-components/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CourseCardComponent } from "./course-card/course-card.component";
+import { HelloworldComponent } from "./helloworld/helloworld.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        CourseCardComponent,
+        HelloworldComponent
+    ]
 })
 export class AppModule { }
---
+++ Angular17/angular-components/src/app/helloworld/helloworld.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'helloworld',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './helloworld.component.html',
+  styleUrl: './helloworld.component.css'
+})
+export class HelloworldComponent {
+
+}
